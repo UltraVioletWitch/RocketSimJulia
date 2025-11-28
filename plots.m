3569e71255a5a59e50fe7d594069a2668eaed88c
violet <ianpk2001@gmail.com>
2025-11-28 10:00:31 -0600

    added MATLAB plotting

---
+++ plots.m
@@ -0,0 +1,54 @@
+clear; clc; close all;
+
+plotAccelAll = false
+plotAccelAlt = true
+plotAlt = true
+plotTrueAll = false
+plotTrueAlt = true
+
+
+T = csvread("./accelerometer_data.csv");
+set(gca, 'fontsize', 20)
+xlabel("Time (s)")
+width = 2;
+
+figure(1)
+hold on
+
+if plotTrueAll
+    T = csvread("./sim_data.csv");
+    ylabel("Position (m), Velocity (m/s), Acceleration (m/s^2)")
+    plot(T(:,1), T(:,2), 'b-', 'LineWidth', width);
+    plot(T(:,1), T(:,3), 'r-', 'LineWidth', width);
+    plot(T(:,1), T(:,4), 'g-', 'LineWidth', width);
+    l = legend({"Position", "Velocity", "Acceleration"});
+    set(l, 'fontsize', 30)
+end
+if plotTrueAlt
+    T = csvread("./sim_data.csv");
+    ylabel("Position (m), Velocity (m/s), Acceleration (m/s^2)")
+    plot(T(:,1), T(:,2), 'b-', 'LineWidth', width);
+    l = legend({"Position"});
+    set(l, 'fontsize', 30)
+end
+if plotAccelAll
+    T = csvread("./accelerometer_data.csv");
+    ylabel("Position (m), Velocity (m/s), Acceleration (m/s^2)")
+    plot(T(:,1), T(:,2), 'b*', 'LineWidth', width);
+    plot(T(:,1), T(:,3), 'r*', 'LineWidth', width);
+    plot(T(:,1), T(:,4), 'g*', 'LineWidth', width);
+    l = legend({"Position", "Velocity", "Acceleration"});
+    set(l, 'fontsize', 30)
+end
+if plotAccelAlt
+    T = csvread("./accelerometer_data.csv");
+    plot(T(:,1), T(:,2), 'g*', 'LineWidth', width);
+end
+if plotAlt
+    T = csvread("./altimeter_data.csv");
+    ylabel("Position (m)")
+    plot(T(:,1), T(:,2), 'r*', 'LineWidth', width);
+    l = legend({"Position"});
+    set(l, 'fontsize', 30)
+end
+hold off
